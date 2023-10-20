c03767e150a388243aba55fd4c3cf6b4331e54c9
Maria Reyna <maria.reyna2244@gmail.com>
2023-10-20 09:01:09 -0400

    Initial Commit

---
+++ MergeTimes/MergeTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MergeTimesApp.swift
+//  MergeTimes
+//
+//  Created by Maria Reyna on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MergeTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

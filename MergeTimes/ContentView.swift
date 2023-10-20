2af3dc76a3e106df9182b81fe5af8d6161914b03
Maria Reyna <maria.reyna2244@gmail.com>
2023-10-20 09:05:27 -0400

    Colors

--- MergeTimes/ContentView.swift
+++ MergeTimes/ContentView.swift
@@ -9,16 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                Image(systemName: "star")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
